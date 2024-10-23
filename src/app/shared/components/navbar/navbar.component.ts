5324b88f7fa38de8bc6010c262fbc8bcace2cadb
Satish Ahirwar <48453902+satish-ahirwar@users.noreply.github.com>
2024-10-23 20:01:47 +0530

    Update navbar.component.ts
    mobile view

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -14,6 +14,18 @@ import { MainService } from '@shared/services/main.service';
       @apply block rounded-md py-1 px-2 transition-all hover:scale-105 hover:text-blue-500 hover:bg-neutral-400/10 active:scale-90;
     }
   }
+  nav {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  display: flex;
+  height: 3.5rem;
+  justify-content: center;
+  background-color: rgba(38, 38, 38, 0.5);
+  padding: 0 1.25rem;
+  color: #A3A3A3;
+  backdrop-blur: 0.25rem;
+}
 `,
 })
 export class NavbarComponent {
