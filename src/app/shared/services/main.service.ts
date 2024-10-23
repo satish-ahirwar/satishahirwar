2b4a7a1dc16ca5c036d581c6701f8c6afed4089d
Satish Ahirwar <48453902+satish-ahirwar@users.noreply.github.com>
2024-10-23 19:22:45 +0530

    Update main.service.ts
    added changes to read the data from drive.

--- src/app/shared/services/main.service.ts
+++ src/app/shared/services/main.service.ts
@@ -10,6 +10,10 @@ export class MainService {
   cvName: string = "Satish-Ahirwar.pdf";
   cvPath: string = "../../../../assets/data/";
   private http = inject(HttpClient);
+  EXTERNAL_DRIVE_URL: string = "https://drive.google.com/uc?id=";
+  EXP_DOC: string = this.EXTERNAL_DRIVE_URL + "1JIZePU9fMgJZjuyAoQ9ee4asTtYudJAY";
+  PROJECT_DOC: string = this.EXTERNAL_DRIVE_URL + "1WWfXktGOYVbLnAQxSBTgHi7YrHxd7bhU";
+  
   getDemographics() {
     return this.http.get<User>(`${this.DATA_QUERY}/demographics.json`);
   }
@@ -17,10 +21,12 @@ export class MainService {
     return this.http.get<string[]>(`${this.DATA_QUERY}/skills.json`);
   }
   getExperience() {
-    return this.http.get<Experience[]>(`${this.DATA_QUERY}/experience.json`);
+    //return this.http.get<Experience[]>(`${this.DATA_QUERY}/experience.json`);
+    return this.http.get<Experience[]>(`${this.EXP_DOC}`);
   }
   getProjects() {
-    return this.http.get<Project[]>(`${this.DATA_QUERY}/projects.json`);
+    //return this.http.get<Project[]>(`${this.DATA_QUERY}/projects.json`);
+    return this.http.get<Project[]>(`${this.PROJECT_DOC}`);
   }
   getCV() {
     window.open(this.cvPath + this.cvName, "_blank");
